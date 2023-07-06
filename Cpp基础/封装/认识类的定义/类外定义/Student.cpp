7b345c446ba4d70ca0e2793a44c68a4ecc3958e8
Gaoxiang ha <109344116+Nelasumin@users.noreply.github.com>
2023-07-07 02:13:29 +0800

    Create Student.cpp

---
+++ Cpp基础/封装/认识类的定义/类外定义/Student.cpp
@@ -0,0 +1,39 @@
+// Student.cpp 文件
+#include "Student.h"// 记得添加类头文件
+
+Student::Student()
+{
+	
+}
+Student::~Student()
+{
+	
+}
+
+void Student::setName(string name)
+{
+	this->name = name;
+}
+
+string Student::getName()
+{
+	return name;
+}
+
+void Student::setAge(int age)
+{
+	this->age = age;
+}
+
+int Student::getAge()
+{
+	return age;
+}
+
+
+
+
+
+
+
+
