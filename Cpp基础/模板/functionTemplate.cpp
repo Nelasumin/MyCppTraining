ce0da3a8025dfb3cd39c988c241453f109168889
Nelasumin <spicewwy@outlook.com>
2023-10-20 11:38:10 +0800

    第一次提交

---
+++ Cpp基础/模板/functionTemplate.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std::cout;
+using namespace std::endl;
+
+template<typename T> //模板参数列表
+T add(T x, T y)
+{
+    retrun x+y;
+}
+
+add(T x,T y)
+{
+
+}
+
+
+void test()
+{
+    int ia = 3, ib =4;
+    double da = 3.3,db = 8.8;
+    string s1 = "hello" ,s2 ="No hello";
+}
+
+
+int main()
+{
+    test();
+    return 0;
+}
