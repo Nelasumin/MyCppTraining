b4a950ccbc9d75b52d2e39eedd57ba7383da6e78
Nelasumin <spicewwy@outlook.com>
2023-10-21 11:13:48 +0800

    测试

--- Cpp基础/模板/functionTemplate.cpp
+++ Cpp基础/模板/functionTemplate.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <string>
 
-using namespace std::cout;
-using namespace std::endl;
+
 
 template<typename T> //模板参数列表
 T add(T x, T y)
-{
-    retrun x+y;
+{   
+    std::cout << "T add(T,T)" << std::endl;
+    return x+y;
 }
-
-add(T x,T y)
-{
-
+// int 类型模板和类型都有的情况下 会调用类型
+int add(int x,int y)
+{   
+    std::cout << "int add(T,T)" << std::endl;
+    return x+y;
 }
 
 
@@ -19,11 +21,13 @@ void test()
 {
     int ia = 3, ib =4;
     double da = 3.3,db = 8.8;
-    string s1 = "hello" ,s2 ="No hello";
+    std::string s1 = "hello" ,s2 ="No hello";
+    std::cout << "add(ia, ib) =" <<add(ia, ib) <<std::endl;
+    std::cout << "add(s1, s2) =" <<add(s1, s2) <<std::endl;
 }
 
 
-int main()
+int main(int argc,char **argv)
 {
     test();
     return 0;
